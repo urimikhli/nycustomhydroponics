b2db435ab114d7155f89e16b5c2a3ed203b26e5f
Uri Mikhli <urimikhli@gmail.com>
2012-07-19 00:09:14 -0400

    Initial create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NYCustomHydroponics::Application.config.secret_token = 'bb722b025e85737982b379129d6c003a945fccea152527dec2bc234e2f20f56bb12d4c23d124c56ea0dcaa9d9324c43e9eb4af483a77b8c31f365d66df765f41'
