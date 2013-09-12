fd46ef1be1246040ee892de0556305b2ecf88ef3
Uri Mikhli <urimikhli@gmail.com>
2013-09-12 00:26:08 -0400

    centralize and make available golobaly the video arrays

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def videos_array
+    @vid_title_array = ["First Video: week 2","Second Video: week 3","Third Video: week 4","Fourth Video: Week 5","Fifth Video: Week 6","Sixth Video: week 7","Seventh Video: Week 8","Eighth Video: Week 9","Ninth Video: Week 10"]
+
+    @vid_src_array = ["https://www.youtube.com/embed/El2Jk4VwOpg?feature=player_detailpage","https://www.youtube.com/embed/Fd8KiWwwLb0?feature=player_detailpage","https://www.youtube.com/embed/wg3D1Bomjo4?feature=player_detailpage","https://www.youtube.com/embed/T-sxjN6DyEI?feature=player_detailpage","https://www.youtube.com/embed/3vNG7BiBCmA?feature=player_detailpage","https://www.youtube.com/embed/ijlnYZIwELM?feature=player_detailpage","https://www.youtube.com/embed/2Dtjmk9PvFs?feature=player_detailpage","https://www.youtube.com/embed/iDRdKnBKeJ8?feature=player_detailpage","https://www.youtube.com/embed/zrZPWK5Luws?feature=player_detailpage"]
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @title ="Home"
+    videos_array
   end
 
   def contact
@@ -18,12 +19,11 @@ class PagesController < ApplicationController
 
   def videos
     @title ="Videos"
-    #@video_src = "https://www.youtube.com/embed/El2Jk4VwOpg?feature=player_detailpage"
-    #@video_title = "First Video: week 2" 
+    videos_array
 
-    @vid_title_array = ["First Video: week 2","Second Video: week 3","Third Video: week 4","Fourth Video: Week 5","Fifth Video: Week 6","Sixth Video: week 7","Seventh Video: Week 8","Eighth Video: Week 9","Ninth Video: Week 10"]
+    #@vid_title_array = ["First Video: week 2","Second Video: week 3","Third Video: week 4","Fourth Video: Week 5","Fifth Video: Week 6","Sixth Video: week 7","Seventh Video: Week 8","Eighth Video: Week 9","Ninth Video: Week 10"]
 
-    @vid_src_array = ["https://www.youtube.com/embed/El2Jk4VwOpg?feature=player_detailpage","https://www.youtube.com/embed/Fd8KiWwwLb0?feature=player_detailpage","https://www.youtube.com/embed/wg3D1Bomjo4?feature=player_detailpage","https://www.youtube.com/embed/T-sxjN6DyEI?feature=player_detailpage","https://www.youtube.com/embed/3vNG7BiBCmA?feature=player_detailpage","https://www.youtube.com/embed/ijlnYZIwELM?feature=player_detailpage","https://www.youtube.com/embed/2Dtjmk9PvFs?feature=player_detailpage","https://www.youtube.com/embed/iDRdKnBKeJ8?feature=player_detailpage","https://www.youtube.com/embed/zrZPWK5Luws?feature=player_detailpage"]
+    #@vid_src_array = ["https://www.youtube.com/embed/El2Jk4VwOpg?feature=player_detailpage","https://www.youtube.com/embed/Fd8KiWwwLb0?feature=player_detailpage","https://www.youtube.com/embed/wg3D1Bomjo4?feature=player_detailpage","https://www.youtube.com/embed/T-sxjN6DyEI?feature=player_detailpage","https://www.youtube.com/embed/3vNG7BiBCmA?feature=player_detailpage","https://www.youtube.com/embed/ijlnYZIwELM?feature=player_detailpage","https://www.youtube.com/embed/2Dtjmk9PvFs?feature=player_detailpage","https://www.youtube.com/embed/iDRdKnBKeJ8?feature=player_detailpage","https://www.youtube.com/embed/zrZPWK5Luws?feature=player_detailpage"]
 
   end
 
